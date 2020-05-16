1087689ed40014b01af888955036e28b263e2aa0
vasilisa <vasilisaskv@gmail.com>
2020-05-16 12:27:06 +0100

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app     = express()
-const path    = require('path')
-const port    = process.env.PORT || 3001
+const express = require('express');
+const path    = require('path');
+const app     = express();
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')));
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+// app.listen(9000);
 app.listen(port, () => console.log("Listening on Port", port)) 
