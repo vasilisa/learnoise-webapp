7375d4a5eb4400e27486ad812a6f247e42b7542b
vasilisa <vasilisaskv@gmail.com>
2020-05-16 12:38:57 +0100

    server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 // app.listen(9000);
-app.listen(port, () => console.log("Listening on Port", port)) 
+app.listen(port, () => console.log("Listening on Port", port));  
