daf3e8a68b3d83a83f79b2ac96b3ea0bb3330829
vasilisa <vasilisaskv@gmail.com>
2020-05-16 13:02:14 +0100

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app     = express();
 // app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 -app.get('/', function (req, res) {
-+app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+	+app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ 	});
+
+});   
 
 // app.listen(9000);
 app.listen(port, () => console.log("Listening on Port", port));  
